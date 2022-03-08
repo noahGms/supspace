c035ba181dd040509f8ec0c098c043a14da6553f
noahGms <noah@keshin.net>
2022-03-08 11:25:12 +0100

    setup default color mode of chakra-ui

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { theme } from "./lib/theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>,
--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,5 +1,9 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const colors = {};
+export const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
 
-export const theme = extendTheme({ colors });
+export const theme = extendTheme({ colors, config });
